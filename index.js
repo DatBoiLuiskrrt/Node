c5b12f3891f65d2262ab86bae3b1e07af08d238b
datboiluiskrrt <datboiluiskrrt@gmail.com>
2022-02-15 07:35:53 -0600

    working on server

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const server = require("./api/server");
+
+server.get("/api/welcome", (req, res) => {
+  res.status(200).json({
+    message: "You done did it maifrend",
+  });
+});
+
 const PORT = process.env.PORT || 6969;
 
 server.listen(PORT, () => {
