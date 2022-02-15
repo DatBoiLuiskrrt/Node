a51296990c76a967c5ac994939e29f687c53843c
datboiluiskrrt <datboiluiskrrt@gmail.com>
2022-02-15 00:29:52 -0600

    Added a nodemon script

--- api/server.js
+++ api/server.js
@@ -9,6 +9,11 @@ server.use("/", (req, res) => {
     message: "Henlo and welcom",
   });
 });
+server.use("/api/", (req,res) => {
+  res.status(200).json({
+    message: "You are inside /api/",
+  })
+})
 
 server.use("*", (req, res) => {
   res.status(404).json({
